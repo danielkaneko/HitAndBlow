9c628c8b9cd9fd065e55d02aa4d3bd2335ad005f
kaneko <kaneko@daniel-soft.com>
2022-08-01 16:11:52 +0900

    重複しない4桁のランダム整数を生成するクラスの追加

---
+++ HitAndBlowTest/QuestionNumberGeneratorTest.vb
@@ -0,0 +1,27 @@
+﻿Imports HitAndBlow
+Imports NUnit.Framework
+
+Public Class QuestionNumberGeneratorTest
+    <Test>
+    Public Sub QuestionNumberGeneratorが重複しないこと()
+        Dim qnr As New QuestionNumberGenerator
+        Dim actual As String = qnr.MakeRandomNumber
+        Assert.That(actual.Length, [Is].EqualTo(actual.Distinct.Count))
+    End Sub
+
+    <Test>
+    Public Sub QuestionNumberGeneratorが４桁の数字を返すこと()
+        Dim qnr As New QuestionNumberGenerator
+        Dim actual As String = qnr.MakeRandomNumber
+        Assert.That(actual.Length, [Is].EqualTo(4))
+    End Sub
+
+    <Test>
+    Public Sub QuestionNumberGeneratorが整数を返すこと()
+        Dim qnr As New QuestionNumberGenerator
+        Dim actual As String = qnr.MakeRandomNumber
+        Assert.IsTrue(IsNumeric(actual))
+        Assert.That(actual, [Is].Not.StringContaining("."))
+    End Sub
+
+End Class
