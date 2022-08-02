1859362be6c8a5c86e321df51c64b7495f38fbd8
kaneko <kaneko@daniel-soft.com>
2022-08-02 14:39:35 +0900

    入力した数値に対するHIT数を表示する

--- HitAndBlow/Main.vb
+++ HitAndBlow/Main.vb
@@ -1,15 +1,23 @@
 ﻿Module Main
 
     Sub Main()
+        Dim qnr As New QuestionNumberGenerator
         Dim dic As New DigitIntegerCheck
         Dim noc As New NumbersOnlyCheck
+        Dim hc As New HitCounter
+        Dim questionNumber As String = qnr.MakeRandomNumber
         Dim answerNumber As String
+        Dim hit As Integer
 
         '数値入力
         Do
             Console.WriteLine("** 数値を入力してください **")
             answerNumber = Console.ReadLine()
         Loop While Not noc.IsNumbersOnly(answerNumber) AndAlso Not dic.IsFourDigitInteger(answerNumber)
+
+        hit = hc.CountHit(answerNumber, questionNumber)
+        Console.WriteLine(hit & "HIT　")
+
     End Sub
 
 End Module
---
+++ HitAndBlowTest/HitCounterTest.vb
@@ -0,0 +1,10 @@
+﻿Imports NUnit.Framework
+Imports HitAndBlow
+Public Class HitCounterTest
+    <Test>
+    Public Sub HitCounterが桁と数字が一致していた数を返すこと()
+        Dim hc As New HitCounter
+        Dim actual As Integer = hc.CountHit("1234", "1244")
+        Assert.That(actual, [Is].EqualTo(3))
+    End Sub
+End Class
