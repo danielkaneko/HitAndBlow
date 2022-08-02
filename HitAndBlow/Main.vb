9c841c256a5e835b2c9f72d2848a5fc9294a44ad
kaneko <kaneko@daniel-soft.com>
2022-08-02 14:23:37 +0900

    答えの数字を入力できるようにする

--- HitAndBlow/Main.vb
+++ HitAndBlow/Main.vb
@@ -1,7 +1,15 @@
 ﻿Module Main
 
     Sub Main()
+        Dim dic As New DigitIntegerCheck
+        Dim noc As New NumbersOnlyCheck
+        Dim answerNumber As String
 
+        '数値入力
+        Do
+            Console.WriteLine("** 数値を入力してください **")
+            answerNumber = Console.ReadLine()
+        Loop While Not noc.IsNumbersOnly(answerNumber) AndAlso Not dic.IsFourDigitInteger(answerNumber)
     End Sub
 
 End Module
---
+++ HitAndBlowTest/DigitIntegerCheckTest.vb
@@ -0,0 +1,31 @@
+﻿Imports NUnit.Framework
+Imports HitAndBlow
+Public Class DigitIntegerCheckTest
+    <Test>
+    Public Sub IsFourDigitIntegerが4桁の整数を通すこと()
+        Dim dic As New DigitIntegerCheck
+        Dim actual As Boolean = dic.IsFourDigitInteger("1234")
+        Assert.IsTrue(actual)
+    End Sub
+
+    <Test>
+    Public Sub DIsFourDigitIntegerが少数を通さないこと()
+        Dim dic As New DigitIntegerCheck
+        Dim actual As Boolean = dic.IsFourDigitInteger("12.3")
+        Assert.IsFalse(actual)
+    End Sub
+
+    <Test>
+    Public Sub IsFourDigitIntegerが4桁以上の数字を通さないこと()
+        Dim dic As New DigitIntegerCheck
+        Dim actual As Boolean = dic.IsFourDigitInteger("12345")
+        Assert.IsFalse(actual)
+    End Sub
+
+    <Test>
+    Public Sub IsFourDigitIntegerが4桁以下の数字を通さないこと()
+        Dim dic As New DigitIntegerCheck
+        Dim actual As Boolean = dic.IsFourDigitInteger("123")
+        Assert.IsFalse(actual)
+    End Sub
+End Class
