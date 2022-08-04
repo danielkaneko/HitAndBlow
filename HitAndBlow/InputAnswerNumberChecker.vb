f1d915e33a7174d904f967d9adbc0198c541cb7b
kaneko <kaneko@daniel-soft.com>
2022-08-04 10:16:17 +0900

    メソッドにコメントがなかったため追加

--- HitAndBlow/InputAnswerNumberChecker.vb
+++ HitAndBlow/InputAnswerNumberChecker.vb
@@ -1,9 +1,20 @@
 ﻿Public Class InputAnswerNumberChecker
 
+    ''' <summary>
+    ''' 入力された数字がShowAnswerかどうか調べる入力チェック
+    ''' </summary>
+    ''' <param name="inputContents">入力した数字</param>
+    ''' <returns>入力された数字がShowAnswerであればTrue、そうでなければFalse</returns>
     Public Function IsInputShowAnswer(inputContents As String) As Boolean
         Return inputContents.Equals("ShowAnswer")
     End Function
 
+    ''' <summary>
+    ''' 入力された数字が回答として適切かどうか調べる入力チェック
+    ''' </summary>
+    ''' <param name="inputContents">入力した数字</param>
+    ''' <param name="questionNumber">答えの数字</param>
+    ''' <returns>回答として適切であればTrue、そうでなければFalse </returns>
     Public Function IsInputAnswerNumber(inputContents As String, questionNumber As String) As Boolean
         Dim dic As New DigitIntegerCheck
         Dim noc As New NumbersOnlyCheck
