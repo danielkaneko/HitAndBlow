d904d1f21cd575eb887332622396c98ed33275e8
kaneko <kaneko@daniel-soft.com>
2022-08-04 10:48:06 +0900

    適切な変数名に修正

--- HitAndBlow/InputAnswerNumberChecker.vb
+++ HitAndBlow/InputAnswerNumberChecker.vb
@@ -3,27 +3,27 @@
     ''' <summary>
     ''' 入力された数字がShowAnswerかどうか調べる入力チェック
     ''' </summary>
-    ''' <param name="inputContents">入力した数字</param>
+    ''' <param name="inputContent">入力した数字</param>
     ''' <returns>入力された数字がShowAnswerであればTrue、そうでなければFalse</returns>
-    Public Function IsInputShowAnswer(inputContents As String) As Boolean
-        Return "ShowAnswer".Equals(inputContents)
+    Public Function IsInputShowAnswer(inputContent As String) As Boolean
+        Return "ShowAnswer".Equals(inputContent)
     End Function
 
     ''' <summary>
     ''' 入力された数字が回答として適切かどうか調べる入力チェック
     ''' </summary>
-    ''' <param name="inputContents">入力した数字</param>
+    ''' <param name="inputContent">入力した数字</param>
     ''' <param name="questionNumber">答えの数字</param>
     ''' <returns>回答として適切であればTrue、そうでなければFalse </returns>
-    Public Function IsInputAnswerNumber(inputContents As String, questionNumber As String) As Boolean
+    Public Function IsInputAnswerNumber(inputContent As String, questionNumber As String) As Boolean
         Dim dic As New DigitIntegerCheck
         Dim noc As New NumbersOnlyCheck
 
-        If IsInputShowAnswer(inputContents) Then
+        If IsInputShowAnswer(inputContent) Then
             Console.WriteLine("答えは" & questionNumber)
             Return False
         Else
-            Return noc.IsNumbersOnly(inputContents) AndAlso dic.IsFourDigitInteger(inputContents)
+            Return noc.IsNumbersOnly(inputContent) AndAlso dic.IsFourDigitInteger(inputContent)
         End If
     End Function
 
