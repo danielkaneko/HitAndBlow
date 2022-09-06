3abb5098a8f9f07722c57389e76b280346516e12
kaneko <kaneko@daniel-soft.com>
2022-09-06 15:08:01 +0900

    意味のある入力かチェックするメソッドのテスト名をシンプルに見やすく変更

--- HitAndBlowTest/MeaningfulInputCheckTest.vb
+++ HitAndBlowTest/MeaningfulInputCheckTest.vb
@@ -2,14 +2,14 @@
 Imports HitAndBlow
 Public Class MeaningfulInputCheckTest
     <Test>
-    Public Sub IsMeaningfulInputが指定桁の整数の時はTrueを返す()
+    Public Sub 渡された数字が指定桁の整数ならばTrueを返す()
         Dim mic As New MeaningfulInputCheck
         Dim actual As Boolean = mic.IsMeaningfulInput("1234", "1234", 4)
         Assert.IsTrue(actual)
     End Sub
 
     <Test>
-    Public Sub IsMeaningfulInputがgiveupの時はTrueを返す()
+    Public Sub 渡された値がgiveupならばTrueを返す()
         Dim mic As New MeaningfulInputCheck
         Dim actual As Boolean = mic.IsMeaningfulInput("giveup", "1234", 4)
         Assert.IsTrue(actual)
@@ -18,7 +18,7 @@ Public Class MeaningfulInputCheckTest
     <TestCase("giveeup", "1234", 4)>
     <TestCase("abc", "1234", 4)>
     <TestCase("123", "1234", 4)>
-    Public Sub IsMeaningfulInputがgiveupでなく指定桁の整数でもない場合はFalseを返す(answerNumber As String, questionNumber As String, digitNumber As Integer)
+    Public Sub 渡された値がgiveupでなく指定桁の整数でもない場合はFalseを返す(answerNumber As String, questionNumber As String, digitNumber As Integer)
         Dim mic As New MeaningfulInputCheck
         Dim actual As Boolean = mic.IsMeaningfulInput(answerNumber, questionNumber, digitNumber)
         Assert.IsFalse(actual)
