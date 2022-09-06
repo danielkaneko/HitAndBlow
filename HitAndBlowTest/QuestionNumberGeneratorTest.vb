f4e7db73a68e6409502c4bd59773c17116f5c77d
kaneko <kaneko@daniel-soft.com>
2022-09-06 15:07:01 +0900

    答えの数字を生成するメソッドのテスト名をシンプルに見やすく変更

--- HitAndBlowTest/QuestionNumberGeneratorTest.vb
+++ HitAndBlowTest/QuestionNumberGeneratorTest.vb
@@ -3,23 +3,23 @@ Imports NUnit.Framework
 
 Public Class QuestionNumberGeneratorTest
     <Test>
-    Public Sub QuestionNumberGeneratorが重複しないこと()
+    Public Sub 生成した数字に重複がないか()
         Dim qnr As New QuestionNumberGenerator
         Dim actual As String = qnr.MakeRandomNumber(4)
         Assert.That(actual.Length, [Is].EqualTo(actual.Distinct.Count))
     End Sub
 
-    <Testcase(3, 3)>
-    <Testcase(6, 6)>
-    <Testcase(10, 10)>
-    Public Sub QuestionNumberGeneratorが指定桁の数字を返すこと(challengeDigit As Integer, answerDigit As Integer)
+    <TestCase(3, 3)>
+    <TestCase(6, 6)>
+    <TestCase(10, 10)>
+    Public Sub 生成した数字が指定桁の数字であるか(challengeDigit As Integer, answerDigit As Integer)
         Dim qnr As New QuestionNumberGenerator
         Dim actual As String = qnr.MakeRandomNumber(challengeDigit)
         Assert.That(actual.Length, [Is].EqualTo(answerDigit))
     End Sub
 
     <Test>
-    Public Sub QuestionNumberGeneratorが整数を返すこと()
+    Public Sub 生成した数字が整数であるか()
         Dim qnr As New QuestionNumberGenerator
         Dim actual As String = qnr.MakeRandomNumber(4)
         Assert.IsTrue(IsNumeric(actual))
